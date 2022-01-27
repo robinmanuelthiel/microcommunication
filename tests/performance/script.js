e8285b74fd863100bf5080a95fcc58b17f80002d
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2022-01-27 14:58:12 +0100

    Add options to k6

--- tests/performance/script.js
+++ tests/performance/script.js
@@ -1,6 +1,14 @@
 import http from "k6/http";
 import { sleep } from "k6";
 
+export const options = {
+  stages: [
+    { duration: "30s", target: 20 },
+    { duration: "1m30s", target: 10 },
+    { duration: "20s", target: 0 },
+  ],
+};
+
 export default function () {
   http.get("http://localhost:8090/api/random/dice");
   sleep(1);
